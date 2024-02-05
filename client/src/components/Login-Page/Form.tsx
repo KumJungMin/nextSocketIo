a15e0bff501921f89443bd1478a862b5116b05fb
KumJungMin <rmawjdals@naver.com>
2024-02-05 21:06:44 +0900

    feat: form 제출 이벤트 추가하기

--- client/src/components/Login-Page/Form.tsx
+++ client/src/components/Login-Page/Form.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { handleSubmit } from "@/lib/fetchers";
 import Avatar from "./Avatar";
 
 function Form() {
+  const router = useRouter();
   const [avatarId, setAvatarId] = useState(
     Math.random().toString(36).substring(7)
   );
+
   return (
-    <form className="flex flex-col gap-5">
+    <form onSubmit={(e) => handleSubmit(e)} className="flex flex-col gap-5">
       <Avatar avatarId={avatarId} setAvatarId={setAvatarId} />
       <div className="flex flex-col xl:flex-row gap-5">
         <div className="form-control w-full">
---
+++ client/src/lib/fetchers.ts
@@ -0,0 +1,9 @@
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+export async function handleSubmit(e: any, router: AppRouterInstance) {
+  e.preventDefault();
+  try {
+  } catch (error) {
+    console.error(error);
+  }
+}
