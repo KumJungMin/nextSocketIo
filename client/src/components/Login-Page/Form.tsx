83ce06916b80704dcbf2f87da299cb792d3fa004
KumJungMin <rmawjdals@naver.com>
2024-02-05 20:11:44 +0900

    feat: form 추가하기

---
+++ client/src/components/Login-Page/Form.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useState } from "react";
+import Avatar from "./Avatar";
+
+function Form() {
+  const [avatarId, setAvatarId] = useState(
+    Math.random().toString(36).substring(7)
+  );
+  return (
+    <form className="flex flex-col gap-5">
+      <Avatar avatarId={avatarId} setAvatarId={setAvatarId} />
+      <div className="flex flex-col xl:flex-row gap-5">
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="label-text text-lg">이름</span>
+          </label>
+          <input
+            type="text"
+            placeholder="이름"
+            className="input input-bordered w-full"
+            required
+          />
+        </div>
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="label-text text-lg">이메일</span>
+          </label>
+          <input
+            type="email"
+            placeholder="이메일"
+            className="input input-bordered w-full"
+            required
+          />
+        </div>
+      </div>
+      <button className="btn btn-primary w-full">가입하기</button>
+    </form>
+  );
+}
+
+export default Form;
