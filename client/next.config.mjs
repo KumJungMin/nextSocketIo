110d1cbee3ede0ef73f5759c1221010d595083ef
KumJungMin <rmawjdals@naver.com>
2024-02-05 20:10:39 +0900

    feat: 아바타 영역 추가하기

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["robohash.org"], // 이미지를 가져올 도메인을 추가
+  },
+};
 
 export default nextConfig;
---
+++ client/src/components/Login-Page/Avatar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { AvatarProps } from "@/types";
+import Image from "next/image";
+import React from "react";
+
+function Avatar({ avatarId, setAvatarId }: AvatarProps) {
+  return (
+    <div
+      onClick={() => setAvatarId((Math.random() * 20).toFixed())}
+      className="avatar cursor-pointer mx-auto mb-5 tooltip"
+      data-tip="클릭해서 아바타 재생성하기"
+    >
+      <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+        <Image
+          src={`https://robohash.org/${avatarId}.png`}
+          width={256}
+          height={256}
+          alt="avatar"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Avatar;
