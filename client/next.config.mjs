b2462aff9904c42152a2dc3ffb6944b9300cca2e
KumJungMin <rmawjdals@naver.com>
2024-02-05 21:23:22 +0900

    chore: 몽고 db 연결하기

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     domains: ["robohash.org"], // 이미지를 가져올 도메인을 추가
+  },
+  // !! rewrites는 next.js에서 제공하는 기능으로,
+  // !! 클라이어트에서 요청하는 URL을 서버에서 다른 URL로 리다이렉트할 수 있게 해준다.
+  async rewrites() {
+    return [
+      {
+        source: "/:path",
+        destination: "http://localhost:4000/:path",
+      },
+    ];
   },
 };
 
--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,23 @@
-import express from 'express';
+import express from "express";
 import "dotenv/config";
-import http from 'http';
-import cors from 'cors';
+import http from "http";
+import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 const server = http.createServer(app);
-const {PORT} = process.env || 4000;
+const { PORT } = process.env || 4000;
 
 app.use(cors());
 
+// 몽고 DB 연결
+mongoose.connect(process.env.MONGO_URL!);
+const db = mongoose.connection;
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("DB connected");
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
